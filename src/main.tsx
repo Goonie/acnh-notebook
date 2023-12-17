35f5c6f810f9ddbe78b610583a72e28f45707a9a
Paul <github@goonie.nl>
2023-12-17 20:32:24 +0100

    Set up app structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { App } from './App';
+
+const rootNode = document.getElementById('react-app');
+
+if (!rootNode) {
+  throw new Error("The app's root node can not be found");
+}
+
+ReactDOM.createRoot(rootNode).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
